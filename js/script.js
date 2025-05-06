233d3c57a86ca73015fd9a997eeaf626be6a5729
Animation User <auser@150-253-29-65.wifi.mcad.edu>
2025-05-06 14:48:05 -0500

    Detailed description of what changes were made

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 
-const guestBar = document.getElementById('guest-bar');
-const qrCodeContainer = document.getElementById('qr-code-container');
+document.addEventListener('DOMContentLoaded', () => {
+  const guestBar = document.getElementById('guest-bar');
+  const qrCodeContainer = document.getElementById('qr-code-container');
 
+  guestBar.addEventListener('click', function() {
+      if (qrCodeContainer.style.display === 'none' || qrCodeContainer.style.display === '') {
+          qrCodeContainer.style.display = 'block';  
+      } else {
+          qrCodeContainer.style.display = 'none';   
+      }
+  });
 
-guestBar.addEventListener('click', function() {
-
-    if (qrCodeContainer.style.display === 'none' || qrCodeContainer.style.display === '') {
-        qrCodeContainer.style.display = 'block';  
-    } else {
-        qrCodeContainer.style.display = 'none';   
-    }
-});
- document.addEventListener('DOMContentLoaded', () => {
   const audio = document.getElementById('bg-audio');
-  if (audio) {
-    audio.play().catch(() => {
-     
-    });
+  const playbutton = document.getElementById('play-audio-button');
+  if (audio && playbutton) {
+      playbutton.addEventListener('click', () => {
+          audio.play().catch(error => {
+              console.error('Failed to start audio playback', error);
+          });
+      });
   }
 });
